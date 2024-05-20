0821362cd8eb0f58431f120b679c1e26e03b63f6
vit <kanada109@gmail.com>
2024-05-20 12:46:09 +0300

    configured postgresql and added: dotenv and .env

--- netology_pd_diplom/netology_pd_diplom/settings.py
+++ netology_pd_diplom/netology_pd_diplom/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,11 +24,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=hs6$#5om031nujz4staql9mbuste=!dc^6)4opsjq!vvjxzj@'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', default=False)
 
+# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
 ALLOWED_HOSTS = ['*']
 
 # Application definition
@@ -76,13 +82,14 @@ WSGI_APPLICATION = 'netology_pd_diplom.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': os.getenv('DB_ENGINE', default="django.db.backends.sqlite3"),
+        'NAME': os.getenv('DB_NAME', default="db.sqlite3"),
+        'HOST': os.getenv('DB_HOST', default="localhost"),
+        'PORT': os.getenv('DB_PORT', default="5432"),
+        'USER': os.getenv('DB_USER', default="user"),
+        'PASSWORD': os.getenv('DB_PASSWORD', default="password"),
     }
-
-
 }
 
 # Password validation
@@ -120,18 +127,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
 
 AUTH_USER_MODEL = 'backend.User'
 
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_USE_TLS = True
 
 EMAIL_HOST = 'smtp.mail.ru'
 
-EMAIL_HOST_USER = 'estgge@mail.ru'
-EMAIL_HOST_PASSWORD = 'i1yVRZAqYzma9E8HWe99'
-EMAIL_PORT = '465'
-EMAIL_USE_SSL = True
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL')
 SERVER_EMAIL = EMAIL_HOST_USER
 
 REST_FRAMEWORK = {
@@ -148,7 +157,7 @@ REST_FRAMEWORK = {
 
         'rest_framework.authentication.TokenAuthentication',
     ),
-
+    "TEST_REQUEST_DEFAULT_FORMAT": "json",
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
